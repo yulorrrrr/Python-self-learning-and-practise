1958862145069ed09df8c338162d6c1b637ba391
yulorrrrr <aayn021121@gmail.com>
2026-01-14 02:37:02 -0500

    Delete Day04 directory

--- Day04/json1.py
+++
@@ -1,9 +0,0 @@
-
-#translate python to json
-import json
-data = [{"name" : "Christine", "age": 11}]
-json_str = json.dumps(data, ensure_ascii= False)
-print(json_str)
-
-#translate json to python: json.loads 
-
